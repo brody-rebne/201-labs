1b930c96c912d20aa506585f4b8fffbb4a88b8d1
brody-rebne <bsrebne@gmail.com>
2020-01-08 19:03:20 -0800

    refactored existing js and added new questions according to lab 3. created basic css styling. added list of movies to html and made other minor changes for lab 3

--- 02/app.js
+++ 02/app.js
@@ -1,65 +1,83 @@
 'use strict';
 
-var correct = 0;
-var incorrect = 0;
-
 var username = prompt('Welcome! What is your name?');
 alert('Hi, ' + username + ', are you ready to take the BrodyQuiz?');
 
-var nameAnswer = prompt('Did Brody attend a CODE 102 course?');
-if(nameAnswer.toLowerCase() === 'yes' || nameAnswer.toLowerCase() === 'y') {
-  alert('Correct!');
-  //console.log('Correct!');
-  correct++;
-} else {
-  alert('Incorrenct');
-  //console.log('Incorrect');
-  incorrect++;
+var strQ = ['Is Brody over 25 years old?', 'Did Brody previously work as a developer?', 'Was Brody born in Washington?', 'Did Brody attend a CODE 102 course?', 'Is Brody enjoying CODE 201 so far?'];
+var strA = ['no', 'no', 'yes', 'yes', 'yes'];
+var input = '';
+var i = 0;
+var correct = 0;
+
+while(i < strQ.length) {
+  input = prompt(strQ[i]);
+  if (input.toLowerCase() === strA[i]) {
+    alert('Correct!');
+    correct++;
+  } else {
+    alert('Incorrect');
+  }
+  i++;
 }
 
-var ageAnswer = prompt('Is Brody over 25 years old?')
-if(ageAnswer.toLowerCase() === 'no' || nameAnswer.toLowerCase() === 'n') {
-  alert('Correct!');
-  //console.log('Correct!');
+var numA = 24;
+
+var g = 0;
+var maxG = 4;
+
+input = Number(prompt('How old is Brody?' + ' You have ' + (maxG - g) + ' guesses left.'));
+if(input === numA) {
   correct++;
+  g++;
+  alert('Correct! It took you ' + g + ' guesses.');
 } else {
-  alert('Incorrect');
-  //console.log('Incorrect');
-  incorrect++;
+  while(input !== numA) {
+    while(g < maxG) {
+      if(input === numA) {
+        correct++;
+        g++;
+        alert('Correct! It took you ' + g + ' guesses.');
+        break;
+      } else if(input < numA) {
+        g++;
+        input = Number(prompt('Answer too low, try again. You have ' + (maxG - g) + ' guesses left.'));
+      } else if(input > numA) {
+        g++;
+        input = Number(prompt('Answer too high, try again. You have ' + (maxG - g) + ' guesses left.'));
+      } else {
+        g++;
+        input = Number(prompt('Incorrect, try again. You have ' + (maxG - g) + ' guesses left.'));
+      }
+    }
+    break;
+  }
 }
 
-var catsAnswer = prompt('Does Brody have any cats?');
-if(catsAnswer.toLowerCase() === 'no' || catsAnswer.toLowerCase() === 'n') {
-  alert('Correct!');
-  //console.log('Correct!');
+var movieA = ['mon oncle', 'metropolis', '2001: a space odyssey', 'the empire strikes back', 'mulholland dr.', 'the thing', 'the sevent seal', 'flash gordon', 'true stories', 'good time'];
+
+g = 0;
+maxG = 6;
+
+input = prompt('What is one of Brody\'s 10 favorite movies?').toLowerCase();
+if(movieA.includes(input) === true) {
   correct++;
+  g++;
+  alert('Correct! It took you ' + g + ' guesses.');
 } else {
-  alert('Incorrect');
-  //console.log('Incorrect');
-  incorrect++;
+  while(movieA.includes(input) === false) {
+    while(g < maxG) {
+      if(movieA.includes(input) === true) {
+        correct++;
+        g++;
+        alert('Correct! It took you ' + g + ' guesses.');
+        break;
+      } else {
+        g++;
+        input = prompt('Incorrect, try again. You have ' + (maxG - g) + ' guesses left.');
+      }
+    }
+    break;
+  }
 }
 
-var stateAnswer = prompt('Was Brody born in Washington?');
-if(stateAnswer.toLowerCase() === 'yes' || stateAnswer.toLowerCase() === 'y') {
-  alert('Correct!');
-  //console.log('Correct!');
-  correct++;
-} else {
-  alert('Incorrect');
-  //console.log('Incorrect');
-  incorrect++;
-}
-
-var funAnswer = prompt('Is Brody enjoying CODE 201 so far?');
-if(funAnswer.toLowerCase() === 'yes' || funAnswer.toLowerCase() === 'y') {
-  alert('Correct!');
-  //console.log('Correct!');
-  correct++;
-} else {
-  alert('Incorrect');
-  //console.log('Incorrect');
-  incorrect++;
-}
-
-var questions = correct + incorrect;
-alert('Thanks for taking the BrodyQuiz, ' + name + '. You scored a ' + correct + '/' + questions + '!');
+alert('You scored ' + correct + '/7 on the BrodyQuiz.'); //how to not hard code 7?
