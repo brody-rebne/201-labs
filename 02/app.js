406c041b070e6820787c6b116c2be7c0f8afece1
Andrew <shifted6@gmail.com>
2020-01-09 17:18:42 -0800

    Javascript updated with functions for number guessing and multiple-answer questions.

--- 02/app.js
+++ 02/app.js
@@ -2,14 +2,13 @@
 
 var username = prompt('Welcome! What is your name?');
 alert('Hi, ' + username + ', are you ready to take the BrodyQuiz?');
-
 var strQ = ['Is Brody over 25 years old?', 'Did Brody previously work as a developer?', 'Was Brody born in Washington?', 'Did Brody attend a CODE 102 course?', 'Is Brody enjoying CODE 201 so far?'];
 var strA = ['no', 'no', 'yes', 'yes', 'yes'];
 var input = '';
 var i = 0;
 var correct = 0;
 var g = 0;
-var maxG = 4;
+var q = 0;
 
 function askYesNo(questionNo) {
   while(i < strQ.length) {
@@ -20,78 +19,65 @@ function askYesNo(questionNo) {
     } else {
       alert('Incorrect');
     }
+    q++;
     i++;
   }
 }
 
-function askNumber() {
+function askNumber(maxG) {
   var numA = 24;
   input = Number(prompt('How old is Brody?' + ' You have ' + (maxG - g) + ' guesses left.'));
-  if(input === numA) {
-    correct++;
-    g++;
-    alert('Correct! It took you ' + g + ' guesses.');
-  } else {
-    while(input !== numA) {
-      while(g < maxG) {
-        if(input === numA) {
-          correct++;
-          g++;
-          alert('Correct! It took you ' + g + ' guesses.');
-          break;
-        } else if(input < numA) {
-          g++;
-          input = Number(prompt('Answer too low, try again. You have ' + (maxG - g) + ' guesses left.'));
-        } else if(input > numA) {
-          g++;
-          input = Number(prompt('Answer too high, try again. You have ' + (maxG - g) + ' guesses left.'));
-        } else {
-          g++;
-          input = Number(prompt('Incorrect, try again. You have ' + (maxG - g) + ' guesses left.'));
-        }
-      }
+  g++;
+  while(g < maxG) {
+    if(input === numA) {
+      correct++;
+      alert('Correct! It took you ' + g + ' guesses.');
       break;
+    } else {
+      if(input < numA) {
+        input = Number(prompt('Answer too low, try again. You have ' + (maxG - g) + ' guesses left.'));
+        g++
+      } else if(input > numA) {
+        input = Number(prompt('Answer too high, try again. You have ' + (maxG - g) + ' guesses left.'));
+        g++;
+      } else {
+        input = Number(prompt('Incorrect, try again. You have ' + (maxG - g) + ' guesses left.'));
+        g++;
+      }
     }
   }
-} 
+  q++;
+}
 
-function askMulti(){
-  console.log('askMulti working');
+function askMulti(maxG){
   var movieA = ['mon oncle', 'metropolis', '2001: a space odyssey', 'the empire strikes back', 'mulholland dr.', 'the thing', 'the sevent seal', 'flash gordon', 'true stories', 'good time'];
   g = 0;
   maxG = 6;
   input = prompt('What is one of Brody\'s 10 favorite movies?').toLowerCase();
-  if(movieA.includes(input) === true) {
-    correct++;
-    g++;
-    alert('Correct! It took you ' + g + ' guesses.');
-  } else {
-    while(movieA.includes(input) === false) {
-      while(g < maxG) {
-        if(movieA.includes(input) === true) {
-          correct++;
-          g++;
-          alert('Correct! It took you ' + g + ' guesses.');
-          break;
-        } else {
-          g++;
-          input = prompt('Incorrect, try again. You have ' + (maxG - g) + ' guesses left.');
-        }
-      }
+  g++;
+  while(g < maxG) {
+    if(movieA.includes(input) === true) {
+      correct++;
+      alert('Correct! It took you ' + g + ' guesses.');
       break;
+    } else {
+      input = prompt('Incorrect, try again. You have ' + (maxG - g) + ' guesses left.');
+      g++;
     }
   }
+  q++;
 }
+
 for (i = 0; i < strQ.length; i++) {
-  askYesNo(i);
+ askYesNo(i);
 }
 
 for (i = 0; i < 1; i++) {
-  askNumber();
+  askNumber(4);
 }
 
 for (i = 0; i < 1; i++) {
-  console.log("Question 7 coming");
-  askMulti();
+  askMulti(6);
 }
-alert('You scored ' + correct + '/7 on the BrodyQuiz.'); //how to not hard code 7?
+
+alert('You scored ' + correct + '/'+q+' on the BrodyQuiz.');
