9b0a2cef34fba6a2ba0d692004380ac68a3d6fac
Andrew <shifted6@gmail.com>
2020-01-09 16:23:10 -0800

    Converted questions to functions for lab 04.

--- 02/app.js
+++ 02/app.js
@@ -8,76 +8,90 @@ var strA = ['no', 'no', 'yes', 'yes', 'yes'];
 var input = '';
 var i = 0;
 var correct = 0;
-
-while(i < strQ.length) {
-  input = prompt(strQ[i]);
-  if (input.toLowerCase() === strA[i]) {
-    alert('Correct!');
-    correct++;
-  } else {
-    alert('Incorrect');
-  }
-  i++;
-}
-
-var numA = 24;
-
 var g = 0;
 var maxG = 4;
 
-input = Number(prompt('How old is Brody?' + ' You have ' + (maxG - g) + ' guesses left.'));
-if(input === numA) {
-  correct++;
-  g++;
-  alert('Correct! It took you ' + g + ' guesses.');
-} else {
-  while(input !== numA) {
-    while(g < maxG) {
-      if(input === numA) {
-        correct++;
-        g++;
-        alert('Correct! It took you ' + g + ' guesses.');
-        break;
-      } else if(input < numA) {
-        g++;
-        input = Number(prompt('Answer too low, try again. You have ' + (maxG - g) + ' guesses left.'));
-      } else if(input > numA) {
-        g++;
-        input = Number(prompt('Answer too high, try again. You have ' + (maxG - g) + ' guesses left.'));
-      } else {
-        g++;
-        input = Number(prompt('Incorrect, try again. You have ' + (maxG - g) + ' guesses left.'));
-      }
+function askYesNo(questionNo) {
+  while(i < strQ.length) {
+    input = prompt(strQ[i]);
+    if (input.toLowerCase() === strA[i]) {
+      alert('Correct!');
+      correct++;
+    } else {
+      alert('Incorrect');
     }
-    break;
+    i++;
   }
 }
 
-var movieA = ['mon oncle', 'metropolis', '2001: a space odyssey', 'the empire strikes back', 'mulholland dr.', 'the thing', 'the sevent seal', 'flash gordon', 'true stories', 'good time'];
-
-g = 0;
-maxG = 6;
-
-input = prompt('What is one of Brody\'s 10 favorite movies?').toLowerCase();
-if(movieA.includes(input) === true) {
-  correct++;
-  g++;
-  alert('Correct! It took you ' + g + ' guesses.');
-} else {
-  while(movieA.includes(input) === false) {
-    while(g < maxG) {
-      if(movieA.includes(input) === true) {
-        correct++;
-        g++;
-        alert('Correct! It took you ' + g + ' guesses.');
-        break;
-      } else {
-        g++;
-        input = prompt('Incorrect, try again. You have ' + (maxG - g) + ' guesses left.');
+function askNumber() {
+  var numA = 24;
+  input = Number(prompt('How old is Brody?' + ' You have ' + (maxG - g) + ' guesses left.'));
+  if(input === numA) {
+    correct++;
+    g++;
+    alert('Correct! It took you ' + g + ' guesses.');
+  } else {
+    while(input !== numA) {
+      while(g < maxG) {
+        if(input === numA) {
+          correct++;
+          g++;
+          alert('Correct! It took you ' + g + ' guesses.');
+          break;
+        } else if(input < numA) {
+          g++;
+          input = Number(prompt('Answer too low, try again. You have ' + (maxG - g) + ' guesses left.'));
+        } else if(input > numA) {
+          g++;
+          input = Number(prompt('Answer too high, try again. You have ' + (maxG - g) + ' guesses left.'));
+        } else {
+          g++;
+          input = Number(prompt('Incorrect, try again. You have ' + (maxG - g) + ' guesses left.'));
+        }
       }
+      break;
     }
-    break;
+  }
+} 
+
+function askMulti(){
+  console.log('askMulti working');
+  var movieA = ['mon oncle', 'metropolis', '2001: a space odyssey', 'the empire strikes back', 'mulholland dr.', 'the thing', 'the sevent seal', 'flash gordon', 'true stories', 'good time'];
+  g = 0;
+  maxG = 6;
+  input = prompt('What is one of Brody\'s 10 favorite movies?').toLowerCase();
+  if(movieA.includes(input) === true) {
+    correct++;
+    g++;
+    alert('Correct! It took you ' + g + ' guesses.');
+  } else {
+    while(movieA.includes(input) === false) {
+      while(g < maxG) {
+        if(movieA.includes(input) === true) {
+          correct++;
+          g++;
+          alert('Correct! It took you ' + g + ' guesses.');
+          break;
+        } else {
+          g++;
+          input = prompt('Incorrect, try again. You have ' + (maxG - g) + ' guesses left.');
+        }
+      }
+      break;
+    }
   }
 }
+for (i = 0; i < strQ.length; i++) {
+  askYesNo(i);
+}
 
+for (i = 0; i < 1; i++) {
+  askNumber();
+}
+
+for (i = 0; i < 1; i++) {
+  console.log("Question 7 coming");
+  askMulti();
+}
 alert('You scored ' + correct + '/7 on the BrodyQuiz.'); //how to not hard code 7?
